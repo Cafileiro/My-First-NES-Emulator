b34309891d548e2c032385a4960837f461961171
Rafael Iralde <riralde@gmail.com>
2025-12-20 03:07:01 +0100

    feat: Add basic PC  and register

---
+++ Nes.cpp
@@ -0,0 +1,8 @@
+// Program Counter point to the next byte to process in memory
+unsigned short ProgramCounter;
+// math and bitwise operations register
+char A;
+// Index and general purpose register X
+char X;
+// Index and general purpose register Y
+char Y;
